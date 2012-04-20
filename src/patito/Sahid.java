e6757e3830552385f517a9c5258b6c67ee96f6aa
Sahid Villela <sdz1992@gmail.com>
2012-04-20 07:04:49 -0600

    Primer Commit en branch Sahid

---
+++ src/patito/Sahid.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package patito;
+
+/**
+ *
+ * @author drako
+ */
+public class Sahid {
+    public static void main (String args[]){
+        
+        System.out.print("ALGO");
+    }
+    
+}
